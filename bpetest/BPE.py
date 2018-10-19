3c19a467d4466e21cef464538b99906996f69a14
Seonbeom Kim <kymj1@naver.com>
2018-10-19 17:23:36 +0900

    Add files via upload

--- bpetest/BPE.py
+++ bpetest/BPE.py
@@ -12,23 +12,54 @@ def read_pd_format(np_format):
 	return df
 
 
-# 문서 읽어서 bpe를 적용할 수 있는 format으로 변환. 
-# "abc" => "a b c space_symbol pad_symbol pad_symbol"
-def get_word_frequency_dict_for_bpe_from_document(path):
-	space_symbol = '</w>'
-	pad_symbol = '</p>'
+# word:"abc" => "a b c space_symbol"
+def word_split_for_bpe(word, space_symbol='</w>'):
+	return ' '.join(list(word)) + ' ' + space_symbol
 
+
+# document read and padding.
+def read_document_and_pad(path, pad_symbol='</p>'):
 	data = pd.read_csv(path, sep=" ", header=None) # pad value is NaN
-	data.fillna(pad_symbol, inplace=True) # NaN => pad_symbol
+	data.fillna(pad_symbol, inplace=True) # NaN => pad_symbol	
+	return np.array(data)
 
-	# to numpy
-	data = np.array(data)
+
+# 문서를 읽고, bpe 적용.
+def document_preprocess_for_bpe(path, space_symbol='</w>', pad_symbol='</p>', eos_symbol='</e>', merge_info=None):
+	data = read_document_and_pad(path, pad_symbol) #numpy type
+	
+	bpe = []
+	for i, row in enumerate(data):
+		sentence = []
+		for j, word in enumerate(row):
+			if word != pad_symbol: # pad는 처리 안함.
+				# "abc" => "a b c space_symbol"
+				split_word = word_split_for_bpe(word, space_symbol)
+				
+				# merge_info를 이용해서 merge.  "a b c space_symbol" ==> "ab cspace_symbol"
+				merge = merge_a_word(merge_info, split_word)
+
+				# 안합쳐진 부분은 다른 단어로 인식해서 공백기준 split 처리해서 sentence에 extend
+				sentence.extend(merge.split())
+
+		# eos 추가.
+		sentence.append(eos_symbol)
+		bpe.append(sentence)
+		
+	return bpe
+
+
+
+# 문서 읽어서 bpe를 적용할 수 있는 format으로 변환. 
+def get_word_frequency_dict_for_bpe_from_document(path, space_symbol='</w>', pad_symbol='</p>'):
+	data = read_document_and_pad(path, pad_symbol) #numpy type
 	word_frequency_dict = {}
+
 	for i, row in enumerate(data):
 		for j, word in enumerate(row):
 			if word != pad_symbol: # pad는 처리 안함.
 				# "abc" => "a b c space_symbol"
-				data[i][j] = ' '.join(list(word)) + ' ' + space_symbol
+				data[i][j] = word_split_for_bpe(word, space_symbol)
 				
 				# word frequency
 				if data[i][j] in word_frequency_dict:
@@ -77,8 +108,8 @@ def check_merge_info(pairs):
 def make_bpe2idx(word_frequency_dict):
 	bpe2idx = {
 				'UNK':0,
-				'go':1,
-				'eos':2,
+				'</g>':1, #go
+				'</e>':2, #eos
 				'</p>':3
 			}
 	idx = 4
@@ -92,10 +123,9 @@ def make_bpe2idx(word_frequency_dict):
 
 
 
-def get_bpe_information(word_frequency_dict):
+def get_bpe_information(word_frequency_dict, num_merges=10):
 	#word_frequency_dict = {'l o w </w>' : 1, 'l o w e r </w>' : 1, 'n e w e s t </w>':1, 'w i d e s t </w>':1}
-	num_merges = 10
-
+	
 	#merge_info: 합친 정보를 기억하고있다가. 나중에 데이터를 같은 순서로만 합치면 똑같이 됨.
 	merge_info = collections.OrderedDict() # 입력 순서 유지
 	recover_info = collections.OrderedDict()
@@ -119,7 +149,6 @@ def get_bpe_information(word_frequency_dict):
 				break
 	
 	bpe2idx = make_bpe2idx(word_frequency_dict)
-
 	return bpe2idx, merge_info, recover_info
 
 
@@ -133,7 +162,7 @@ def merge_a_word(merge_info, word):
 
 		# 만약 ''.join(info): r</w> 이고, word: 'a r </w>' 이면 w_out은 'a r</w>'가 된다.
 		word = p.sub(''.join(info), word)
-		print(word)
+	return word
 
 
 
@@ -147,12 +176,38 @@ def make_bpe_format_testcode():
 	print('bpe2idx', len(bpe2idx))
 	print('merge_info', len(merge_info))
 	print('recover_info', len(recover_info), '\n\n')
-	
+		
 	merge_a_word(recover_info, "c e m e n t </w>")
-	#split_word_bpe_format(bpe2idx)
 	for i in recover_info:
 		print(i)	
+
+
+def testcode_2():
+	path = "./testdata.en"
+	
+	# 일단 다른 문서들로부터 bpe2idx, merge_info, recover_info를 구했다고 치자.
+	word2idx = get_word_frequency_dict_for_bpe_from_document(path)	
+	bpe2idx, merge_info, recover_info = get_bpe_information(word2idx, num_merges=30)#100
+
+	#여기서부터 시작한다.
+	bpe = document_preprocess_for_bpe(path, merge_info=merge_info) # 2d list not numpy
+	# 쉽게 패딩하기. (pandas로 변환 => fillna 함수로 패딩처리 => numpy로 변환)
+	bpe = pd.DataFrame(bpe)
+	bpe.fillna('</p>', inplace=True) # NaN => pad_symbol	
+	bpe = np.array(bpe)
+	print(bpe)
+
+
+	
+	'''
+	print(word2idx[0])	
+	merge = merge_a_word(merge_info, word2idx[0][0])
+	#merge = merge_a_word(recover_info, word2idx[0][0])
+	print(merge)
+	'''
+
+
 #bpe_testcode()
+#make_bpe_format_testcode()
 
-make_bpe_format_testcode()
-
+testcode_2()
