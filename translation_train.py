a8a78e2610b2d2b9c26861fc21a8e7a355c5c8d1
Seonbeom Kim <kymj1@naver.com>
2018-12-15 01:01:41 +0900

    Add files via upload

--- translation_train.py
+++ translation_train.py
@@ -1,36 +1,36 @@
-#https://arxiv.org/abs/1706.03762 Attention Is All You Need(Transformer)
-#https://arxiv.org/abs/1607.06450 Layer Normalization
-
-import Transformer
-import Decode_helper # greedy, beam
+import tensorflow as tf
 import numpy as np
-import tensorflow as tf # 1.4
-import Inference_utils # metric, inference
-import bucket_data_helper
-import datetime
+import transformer
+import inference_helper
 import os
-import collections
-
-
+import bucket_data_helper
+from tqdm import tqdm
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 train_set_path = './bpe_dataset/train_bucket_concat_dataset.npy'
-valid_set_path = './bpe_dataset/valid_bucket_concat_dataset.npy'
+vali_set_path = './bpe_dataset/valid_bucket_concat_dataset.npy'
 bpe2idx_path = './bpe_dataset/bpe2idx.npy'
 
+#test_set_path = './calc/test_set.csv'
 saver_path = './saver/'
 tensorboard_path = './tensorboard/'
+
+
+warmup_steps = 4000 #//10
 
 
 def load_dictionary(path):
 	data = np.load(path, encoding='bytes').item()
 	return data
 
+def save_dictionary(path, dictionary):
+	np.save(path, dictionary)
+
 
 def train(model, data, lr):
-	loss = 0
 	total_data_len = 0
+	loss = 0
 
 	data.shuffle()
 
@@ -39,56 +39,109 @@ def train(model, data, lr):
 		if batch is 0:
 			break # epoch end
 
-		batch_data, bucket = batch[0], batch[1]
-		total_data_len += len(batch_data)
-	
-		sentence = batch_data[:, :bucket[0]]
-		target = batch_data[:, bucket[0]:]
+		batch_data, encoder_input_length = batch[0], batch[1][0]
+		
+		encoder_input = batch_data[:, :encoder_input_length]
+		decoder_input = batch_data[:, encoder_input_length:-1] 
+		target = batch_data[:, encoder_input_length+1:] # except '</g>'
 
-		train_loss, _ = sess.run([model.train_cost, model.minimize], 
+
+		if total_data_len == 0:
+			train_loss, _, a, b = sess.run([model.train_cost, model.minimize, model.decoder_pred, model.target_pad_mask], 
+			#train_loss, _, a, b = sess.run([model.train_cost, model.minimize, model.decoder_pred_except_eos, model.target_pad_mask], 
+			#train_loss, _ = sess.run([model.train_cost, model.minimize], 
 							{
 								model.lr:lr,
-								model.sentence:sentence, 
+								model.encoder_input:encoder_input, 
+								model.decoder_input:decoder_input,
 								model.target:target, 
 								model.keep_prob:0.9 # dropout rate = 0.1		
 							}
 						)
-		loss += train_loss
+			print("decode\n", a[:1])
+			print("target\n", (target[:1]*b[:1]).astype(np.int32))
 
+		else:	
+			train_loss, _ = sess.run([model.train_cost, model.minimize], 
+		#train_loss, _ = sess.run([model.train_cost, model.minimize], 
+							{
+								model.lr:lr,
+								model.encoder_input:encoder_input, 
+								model.decoder_input:decoder_input,
+								model.target:target, 
+								model.keep_prob:0.9 # dropout rate = 0.1		
+							}
+						)
+
+		total_data_len += len(batch_data)
+		loss += train_loss
+	
 	return loss/total_data_len
 
 
-def validation(model, data, infer_utils_dict):
-	loss = 0
+
+def validation(model, data):
 	total_data_len = 0
-	
+	loss = 0
+
 	while True:
 		batch = data.get_batch()
 		if batch is 0:
 			break # epoch end
 
-		batch_data, bucket = batch[0], batch[1]
-		total_data_len += len(batch_data)
-	
-		sentence = batch_data[:, :bucket[0]]
-		target = batch_data[:, bucket[0]:]
-		
-		vali_loss = sess.run(infer_utils_dict[bucket]['cost'], 
-					{
-						model.sentence:sentence, 
-						model.target:target, 
-						model.keep_prob:1
-					}
-				)
+		batch_data, encoder_input_length = batch[0], batch[1][0]
+		encoder_input = batch_data[:, :encoder_input_length]
+		decoder_input = batch_data[:, encoder_input_length:-1] 
+		target = batch_data[:, encoder_input_length+1:] # except '</g>'
 
-		loss += vali_loss
+		vali_loss = sess.run(model.train_cost, # greedy나 beamsearch embedding로 계산하는것이 엄밀함.
+							{
+								model.encoder_input:encoder_input, 
+								model.decoder_input:decoder_input,
+								model.target:target, 
+								model.keep_prob:1
+							}
+						)
+
+		total_data_len += len(batch_data)
+		loss += vali_loss		
+
 
 	return loss/total_data_len
 
 
+"""
+def test(model, data):
+	batch_size = 256 #// 2
+	correct = 0
 
+	#for i in range( int(np.ceil(len(data)/batch_size)) ):
+	for i in tqdm(range( int(np.ceil(len(data)/batch_size)) ), ncols=50):
+		batch = data[batch_size * i: batch_size * (i + 1)]
+		
+		encoder_input = batch[:, :sentence_length]
+		decoder_input = batch[:, sentence_length:-1]
+		target = batch[:, sentence_length+1:]
 
-def run(model, sess, train_batch, valid_batch, infer_utils_dict,  restore=0):
+		infer_pred = greedy.decode(encoder_input, target_length=target.shape[1])
+		infer_pred_except_eos = sess.run(model.except_eos, {model.feed_for_except_eos:infer_pred})
+		target_except_eos = sess.run(model.except_eos, {model.feed_for_except_eos:target})
+
+		count = utils.correct(infer_pred_except_eos, target_except_eos)
+		correct += count
+		'''
+		print('\ninfer_pred\n', infer_pred[:3], '\n')
+		print('infer_pred_except_eos\n', infer_pred_except_eos[:3], '\n')
+		print('target\n', target[:3], '\n')
+		print('correct:', correct, '\n')
+		'''
+		print('correct:', correct, '\n')
+
+		break
+	return correct/len(data)
+"""
+
+def run(model, trainset, valiset, testset, restore=0):
 	with tf.name_scope("tensorboard"):
 		train_loss_tensorboard = tf.placeholder(tf.float32, name='train_loss')
 		vali_loss_tensorboard = tf.placeholder(tf.float32, name='vali_loss')
@@ -104,102 +157,76 @@ def run(model, sess, train_batch, valid_batch, infer_utils_dict,  restore=0):
 	if not os.path.exists(saver_path):
 		print("create save directory")
 		os.makedirs(saver_path)
-
-
-	if restore != 0:
-		print("restore", restore)
-		model.saver.restore(sess, saver_path+str(restore)+".ckpt")
-		
-		vali_loss = validation(model, valid_batch, infer_utils_dict)
-		print("epoch:", restore, "\tvali_loss:", vali_loss, '\ttime:', datetime.datetime.now())
-
-
 	
+	import datetime
+	print('start', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
 	for epoch in range(restore+1, 20000+1):
 		lr = (embedding_size**-0.5) * min( (epoch**-0.5), (epoch*(warmup_steps**-1.5)) )
+		#lr = 0.01
 
-		train_loss = train(model, train_batch, lr)
-		print("epoch:", epoch, 'train_loss:', train_loss, 'time:', datetime.datetime.now(), 'lr:', lr)
-		
-		if epoch % 10 == 0:
+		train_loss = train(model, trainset, lr)
+		print('train', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+		vali_loss = validation(model, valiset)
+		print('vali', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+		#correct = test(model, testset)
+		#print('test', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+		print("epoch:", epoch, 'train_loss:', train_loss,  'vali_loss:', vali_loss, 'lr:', lr, '\n')
+
+		# tensorboard
+		summary = sess.run(merged, {
+					train_loss_tensorboard:train_loss, 
+					vali_loss_tensorboard:vali_loss,
+					#test_accuracy_tensorboard:accuracy, 
+				}
+		)
+		writer.add_summary(summary, epoch)
+
+		if (epoch) % 100 == 0:
 			model.saver.save(sess, saver_path+str(epoch)+".ckpt")
 
-
-		if epoch % test_epoch == 0:
-			vali_loss = validation(model, valid_batch, infer_utils_dict)
-			print("epoch:", epoch, "train_loss:", train_loss, "\tvali_loss:", vali_loss, '\tlr:', lr, '\ttime:', datetime.datetime.now())
 		
-			#accuracy = test(model, testset)
-			#print("epoch:", epoch, "train_loss:", train_loss, "\tvali_loss:", vali_loss, "\taccuracy:", accuracy, '\tlr:', lr, '\ttime:', time.time()-prev)
 
-			# tensorboard
-			summary = sess.run(merged, {
-						train_loss_tensorboard:train_loss, 
-						vali_loss_tensorboard:vali_loss,
-						#test_accuracy_tensorboard:accuracy, 
-					}
-			)
-			writer.add_summary(summary, epoch)
+		#accuracy = test(model, testset)
 
 
 
 
-	
-		
+
 
 print('Data read')
-train_set = load_dictionary(train_set_path)
-train_batch = bucket_data_helper.bucket_data(train_set, iter=True, batch_token = 16000)
-all_bucket = train_batch.get_all_bucket()
-
-valid_set = load_dictionary(valid_set_path)
-valid_batch = bucket_data_helper.bucket_data(valid_set, iter=True, batch_token = 16000)
-
+train_set_bucket = load_dictionary(train_set_path)
+train_set = bucket_data_helper.bucket_data(train_set_bucket, iter=True, batch_token = 12000) # batch_token // len(sentence||target token) == batch_size
+vali_set_bucket = load_dictionary(vali_set_path)
+vali_set = bucket_data_helper.bucket_data(vali_set_bucket, iter=True, batch_token = 12000) # batch_token // len(sentence||target token) == batch_size
 bpe2idx = load_dictionary(bpe2idx_path)
 
-
-
 embedding_size = 256
-warmup_steps = 1000
-test_epoch = 50
 
-print('Transformer read')
+print("Model read")
 sess = tf.Session()
-model = Transformer.Transformer(
+
+model = transformer.Transformer(
 			sess = sess,
 			voca_size = len(bpe2idx), 
 			embedding_size = embedding_size, 
 			is_embedding_scale = True, 
-			PE_sequence_length = 200,
-			encoder_decoder_stack = 1,
+			PE_sequence_length = 300,
+			encoder_decoder_stack = 1*3,
+			multihead_num = 8,
 			go_idx=bpe2idx['</g>'], 
 			eos_idx=bpe2idx['</e>'], 
-			pad_idx=-1,
+			pad_idx=bpe2idx['</p>'],
 			label_smoothing=0.1
 		)
-
-decode_helper = Decode_helper.greedy_decoder()
-print(decode_helper)
-
-
-# target_length별로 infer utils 선언.
-infer_utils = Inference_utils.utils(model=model, go_idx=bpe2idx['</g>'], eos_idx=bpe2idx['</e>'], pad_idx=-1)
-infer_utils_dict = collections.defaultdict(int) # tuple form으로 key 사용 가능함.
-for bucket in all_bucket:
-	infer_pred_except_eos, infer_embedding = infer_utils.inference(
-				decode_helper=decode_helper, 
-				is_embedding_scale=True, 
-				target_length=bucket[1]
-			)
-	infer_cost = infer_utils.cost(infer_embedding)
-	infer_accuracy = infer_utils.accuracy(infer_pred_except_eos, target_length=bucket[1])
-	infer_dict = {
-				'inference':infer_pred_except_eos, 
-				'cost':infer_cost, 
-				'accuracy':infer_accuracy
-			}
-	infer_utils_dict[bucket] = infer_dict
-	
+greedy = inference_helper.greedy(sess, model, bpe2idx['</g>'])
+utils = inference_helper.utils()
 
 print('run')
-run(model, sess, train_batch, valid_batch, infer_utils_dict, 90)
+run(model, train_set, vali_set, None)
+
+#print(sess.run(model.embedding_table))
+
+#testcode()
+
